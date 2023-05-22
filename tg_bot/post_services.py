1d4030c051d40a672f7d07349c05670e055bff73
Ivan Rodkin <irodkin2002@gmail.com>
2023-05-22 15:50:51 +0300

    add refinement

---
+++ tg_bot/post_services.py
@@ -0,0 +1,13 @@
+from dff.script import Context
+import requests
+
+
+def response_refiner(ctx: Context):
+    last_request = ctx.last_request
+    last_response = ctx.last_response
+    if last_response is None:
+        # nothing to refine
+        return
+    response = requests.post(url='http://response_refinement:8030/refine', json={'question': last_request, 'response': last_response, })
+    model_response = response.json()
+    ctx.set_last_response(model_response['refined_resp'])
